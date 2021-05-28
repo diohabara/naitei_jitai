2aa2cb9f3c1f1acf3411929afb97d6fee8a90633
diohabara <diohabara@gmail.com>
2021-05-28 15:18:03 +0900

    fix: enable tailwindcss with example of https://snowsystem.net/javascript/react/tailwindcss-create-react-app/

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App: React.FC = () => {
+	return (
+		<div className="flex justify-items-center flex-wrap p-4 bg-green-200">
+			<div className="flex justify-center items-center m-4 bg-green-400 h-80 w-80 rounded-lg text-9xl">1</div>
+			<div className="flex justify-center items-center m-4 bg-green-400 h-80 w-80 rounded-lg text-9xl">2</div>
+			<div className="flex justify-center items-center m-4 bg-green-400 h-80 w-80 rounded-lg text-9xl">3</div>
+			<div className="flex justify-center items-center m-4 bg-green-400 h-80 w-80 rounded-lg text-9xl">4</div>
+			<div className="flex justify-center items-center m-4 bg-green-400 h-80 w-80 rounded-lg text-9xl">5</div>
+			<div className="flex justify-center items-center m-4 bg-green-400 h-80 w-80 rounded-lg text-9xl">6</div>
+			<div className="flex justify-center items-center m-4 bg-green-400 h-80 w-80 rounded-lg text-9xl">7</div>
+			<div className="flex justify-center items-center m-4 bg-green-400 h-80 w-80 rounded-lg text-9xl">8</div>
+			<div className="flex justify-center items-center m-4 bg-green-400 h-80 w-80 rounded-lg text-9xl">9</div>
+			<div className="flex justify-center items-center m-4 bg-green-400 h-80 w-80 rounded-lg text-9xl">10</div>
+		</div>
+	);
 }
 
-export default App;
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        blueGray: colors.blueGray,
+        coolGray: colors.coolGray,
+        gray: colors.gray,
+        trueGray: colors.trueGray,
+        warmGray: colors.warmGray,
+        red: colors.red,
+        orange: colors.orange,
+        amber: colors.amber,
+        yellow: colors.yellow,
+        lime: colors.lime,
+        green: colors.green,
+        emerald: colors.emerald,
+        teal: colors.teal,
+        cyan: colors.cyan,
+        lightBlue: colors.lightBlue,
+        indigo: colors.indigo,
+        violet: colors.violet,
+        purple: colors.purple,
+        fuchsia: colors.fuchsia,
+        pink: colors.pink,
+        rose: colors.rose,
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
