317d22ee634b7dc96419f1a1cac9c7f342c11276
TKadoi <diohabara@gmail.com>
2022-03-05 16:14:09 +0900

    Updates

---
+++ precache-manifest.a24886d87a9bd0179b46b9662a3c72d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f62a51ac4f0a83572080",
+    "url": "/naitei_jitai/static/css/main.990e9552.chunk.css"
+  },
+  {
+    "revision": "f62a51ac4f0a83572080",
+    "url": "/naitei_jitai/static/js/main.3c93db75.chunk.js"
+  },
+  {
+    "revision": "161fb60d7c82e96d2502",
+    "url": "/naitei_jitai/static/js/runtime~main.d6edb46d.js"
+  },
+  {
+    "revision": "d2fe8da216b6e8ad7847",
+    "url": "/naitei_jitai/static/js/2.d5a90baf.chunk.js"
+  },
+  {
+    "revision": "0f99ebe3cfcb8805683d",
+    "url": "/naitei_jitai/static/js/3.81acc60e.chunk.js"
+  },
+  {
+    "revision": "6324d5eeb7f77b4d8b793ed98b2b9e11",
+    "url": "/naitei_jitai/index.html"
+  }
+];
